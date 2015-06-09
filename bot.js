e576eac02e4b40565d6af344293d4f0c699a2ed9
456z <haecktim@383z.fritz.box>
2015-06-09 23:58:37 +0200

    endgame test
    endgame test

--- bot.js
+++ bot.js
@@ -3,44 +3,80 @@
 var sDrip = false;
 var toFarm = '#pu11';
 
+var clickBot = setInterval(clickBot,50);
+var buyBot = setInterval(buyBot,10000);
+var dripBot = /*setInterval(dripBot,10000)*/;
+
+
 
 //init
-/*$('.schniptest').remove();
+$('.schniptest').remove();
 $(function(){
 	var child = $('#powerupstore').children();
 	for(var i = 0; i < child.length; i++) {
 
 		var pos = $(child[i]).position();
 		var puId = $(child[i]).prop('id')
-		$('#powerupstore').append('<input class="schniptest" data-pu="'+puId+'" type="NUMBER" min="-1" max="1000" step="1" value="0" size="0" style="float: right;position: absolute;top: '+pos.top+'px;left: -39px;">');
+		$('#powerupstore').append('<div class="schniptest col-sm-3" style="float: right;position: absolute;top: '+(pos.top+25)+'px;right: 65px;"><input class="form-control" data-pu="'+puId+'" type="NUMBER" min="-1" max="1000" step="1" value="0" size="0" ></div>');
 	}
+	var append = '                <div id="dripBotTAH" style="text-align:left;">                	<div>                		<h5>Click bot:</h5>						<div class="form-inline">							<button id="startBot" class="btn btn-primary">Start</button>							<label class="checkbox-inline">								<input type="checkbox" id="showAnimation" value=""> show animation							</label>						</div>						<div>							<label>memory/sec: <label id="memoryPerSec">843 MB</label></label>						</div>						<div>							<label>memory/min: <label id="memoryPerMin">843 MB</label></label>						</div>						<div>							<label>memory/hr: <label id="memoryPerHr">843 MB</label></label>						</div>                	</div>                	<div class="form-inline">                		<div class="form-group">							<label class="checkbox-inline">								<input  class="" type="checkbox" id="autoDrip" value=""> auto drip							</label>							<input type="text" id="autoDripValue" class="form-control" placeholder="drip after x seconds...">						</div>                	</div>                </div>';
+	$('#middleColumn').append(append);
+	var bps = (Math.floor(localStats.bps/1024/1024*100) / 100) + (Math.floor(CoffeeCup.calcBytesPerClick()*20/1024/1024 * 100) / 100);
 
-});*/
+	$('#memoryPerSec').html(bps + " MB");
+	$('#memoryPerMin').html((bps*60) + " MB");
+	$('#memoryPerHr').html((bps*60*60) + " MB");
+	
+	$('#')
+});
 
 /*var set = setInterval(function(){
 	$('#btn-addMem').trigger('click');
 },50);*/
 
-var set = setInterval(function(){
-	var t = CoffeeCup.calcBytesPerClick();
-    localStats.byteCount + t > localStats.memoryCapacity && (t = localStats.memoryCapacity - localStats.byteCount), localStats.byteCount += t
-},50);
 
-var drip = setInterval(function(){
+
+function dripBot(){
   if(sDrip){
 	$('#btn-addGlobalMem').trigger('click');
   }
-},10000);
+}
 
-var checkError = setInterval(function(){
-	if($('#networkError').is(':visible')) {
-		location.reload();
-	}
-},60000);
+function clickBot() {
+	var t = CoffeeCup.calcBytesPerClick();
+    localStats.byteCount + t > localStats.memoryCapacity && (t = localStats.memoryCapacity - localStats.byteCount), localStats.byteCount += t
+}
 
+function buyBot() {
+	var children = $('#powerupstore').find('.storeItem');
+	//for(var i = children.length; i >= 0; i--){
+		var child = $(toFarm);
+		var childId = $(child).prop('id');
 
+		/*var amountToBuy = $('#powerupstore').find('input[data-pu='+childId+']');
+		var oldAmount = $('#'+childId).find('.storeItemAmount').html();*/
+		var price = $('#'+childId).find('.storePrice').text();
+		var max = $('#memoryLimit').find('.amount').text();
+		
+
+		//if($(amountToBuy).val() > 0 || $(amountToBuy).val() == -1) {
+			if(compareBytes(price, max)) {
+				sDrip = true;
+				//break;
+			} else {
+				sDrip = false;
+			}
+			$(child).trigger('click');
+			//if(!increase(child, amountToBuy, oldAmount)) {
+			//	break;
+			//}
+		//}
+	//}
+}
+
+/*
 clearInterval(buy);
-/*var buy = setInterval(function(){
+var buy = setInterval(function(){
 	var children = $('#powerupstore').find('.storeItem');
 	for(var i = children.length; i >= 0; i--){
 		var child = $(children[i]);
@@ -66,32 +102,7 @@ clearInterval(buy);
 	}
 },10000);*/
 
-var buy = setInterval(function(){
-	var children = $('#powerupstore').find('.storeItem');
-	//for(var i = children.length; i >= 0; i--){
-		var child = $(toFarm);
-		var childId = $(child).prop('id');
 
-		/*var amountToBuy = $('#powerupstore').find('input[data-pu='+childId+']');
-		var oldAmount = $('#'+childId).find('.storeItemAmount').html();*/
-		var price = $('#'+childId).find('.storePrice').text();
-		var max = $('#memoryLimit').find('.amount').text();
-		
-
-		//if($(amountToBuy).val() > 0 || $(amountToBuy).val() == -1) {
-			if(compareBytes(price, max)) {
-				sDrip = true;
-				//break;
-			} else {
-				sDrip = false;
-			}
-			$(child).trigger('click');
-			//if(!increase(child, amountToBuy, oldAmount)) {
-			//	break;
-			//}
-		//}
-	//}
-},10000);
 
 function increase(child, amountToBuy, oldAmount) {
 	$(child).trigger('click');
@@ -149,3 +160,10 @@ function compareBytes(first, second) {
 
 	return parseFloat(first) > parseFloat(second);
 }
+
+
+var checkError = setInterval(function(){
+	if($('#networkError').is(':visible')) {
+		location.reload();
+	}
+},60000);
