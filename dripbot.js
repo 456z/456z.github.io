e0558556e24143f17f0811826ef2b96d11a507af
456z <haecktim@383z.fritz.box>
2015-06-10 20:19:06 +0200

    u
    u

--- dripbot.js
+++ dripbot.js
@@ -1,3 +1,3 @@
 	setTimeout(function(){
-			$("head").append("<style id='test_style'></style>");
-	},10000);
+			$("head").append("<style id='test_style'>#dripBotOptions{margin:15px auto auto;border:1px solid;border-radius:5px;background-color:#E2E2E2}body{background-color:#3f3f3f;color:#4fbd4f}.storeItem{background-color:#C9CFDA}#memoryLimit{color:#109BA3}#bps-table-div{padding:5px 0}#bps-table th,td{text-align:center}#bps-table th{border-bottom:1px solid #7263FC}#btn-addMem{margin:-14px 0 13px;width:100px;height:100px}#btn-addMem:hover{width:100px;height:100px}#btn-addMem:active{width:100px;height:100px;background-size:100px;background-position:0 0}#pu11{border-bottom:1px solid;border-top:0}#bpsChartContainer{margin-top:13px}</style>");
+	},1000);
